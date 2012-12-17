35bd341818a360b0932c57867ac61bc23b61a608
Julien Rouhaud <julien.rouhaud@free.fr>
2012-12-17 18:10:59 +0100

    aggregate rows by day, and add ability to log error in dispatch_record function

--- wh_nagios--1.0.sql
+++ wh_nagios--1.0.sql
@@ -1,22 +1,39 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION wh_nagios" to load this file. \quit
 
-CREATE TABLE wh_nagios.hub (
-	id bigserial,
-	data text[]
-);
+SET statement_timeout TO 0;
 
+CREATE TYPE wh_nagios.counters_detail AS (
+    timet timestamp with time zone,
+    value numeric
+);
+ALTER TYPE wh_nagios.counters_detail OWNER TO pgfactory;
+
+CREATE TABLE wh_nagios.hub (
+    id bigserial,
+    data text[]
+);
 ALTER TABLE wh_nagios.hub OWNER TO pgfactory;
-REVOKE ALL ON TABLE wh_nagios.hub FROM public ;
+REVOKE ALL ON TABLE wh_nagios.hub FROM public;
+
+CREATE TABLE wh_nagios.hub_reject (
+    id bigserial,
+    data text[],
+    msg text
+);
+ALTER TABLE wh_nagios.hub_reject OWNER TO pgfactory;
+REVOKE ALL ON TABLE wh_nagios.hub_reject FROM public;
 
 CREATE TABLE wh_nagios.services (
-	label		text,
-	unit		text,
-	state		text,
-	min			numeric,
-	max			numeric,
-	critical	numeric,
-	warning		numeric
+    label            text,
+    unit             text NOT NULL,
+    state            text,
+    min              numeric,
+    max              numeric,
+    critical         numeric,
+    warning          numeric,
+    oldest_record    timestamptz DEFAULT now(),
+    newest_record    timestamptz
 )
 INHERITS (public.services);
 
@@ -28,28 +45,39 @@ REVOKE ALL ON TABLE wh_nagios.services FROM public ;
 
 SELECT pg_catalog.pg_extension_config_dump('wh_nagios.services', '');
 
-CREATE OR REPLACE FUNCTION wh_nagios.dispatch_record() RETURNS boolean
-	LANGUAGE plpgsql
-	AS $$
+
+/* wh_nagios.dispatch_record(boolean)
+Dispatch records from wh_nagios.hub into counters_detail_$ID
+
+$ID is found in wh_nagios.services, with correct hostname,servicedesc and label
+
+@log_error: If true, will report errors and details in wh_nagios.hub_reject
+@return : true if everything went well.
+*/
+CREATE OR REPLACE FUNCTION wh_nagios.dispatch_record(log_error boolean DEFAULT false) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
 DECLARE
-	--Select current lines and lock them so then can be deleted
-	--Use NOWAIT so there can't be two concurrent processes
-	c_hub CURSOR FOR SELECT * FROM wh_nagios.hub FOR UPDATE NOWAIT;
-	r_hub record;
-	service_id integer;
+    --Select current lines and lock them so then can be deleted
+    --Use NOWAIT so there can't be two concurrent processes
+    c_hub CURSOR FOR SELECT * FROM wh_nagios.hub FOR UPDATE NOWAIT;
+    r_hub record;
+    service_id integer;
     i integer;
     cur hstore;
+    msg_err text;
+    servicesrow wh_nagios.services%ROWTYPE;
 BEGIN
 /*
 TODO: Handle seracl 
 
 */
-	BEGIN
-		FOR r_hub IN c_hub LOOP
+    BEGIN
+        FOR r_hub IN c_hub LOOP
             --Check 1 dimension,even number of data and at least 10 vals
-			IF ((array_upper(r_hub.data,2) IS NULL) AND (array_upper(r_hub.data,1) > 9) AND ((array_upper(r_hub.data,1) % 2) = 0)) THEN
+            IF ((array_upper(r_hub.data,2) IS NULL) AND (array_upper(r_hub.data,1) > 9) AND ((array_upper(r_hub.data,1) % 2) = 0)) THEN
                 cur := NULL;
-                --Get all data as hstore
+                --Get all data as hstore,lowercase
                 FOR i IN 1..array_upper(r_hub.data,1) BY 2 LOOP
                     IF (cur IS NULL) THEN
                         cur := hstore(lower(r_hub.data[i]),r_hub.data[i+1]);
@@ -57,54 +85,193 @@ TODO: Handle seracl
                         cur := cur || hstore(lower(r_hub.data[i]),r_hub.data[i+1]);
                     END IF;
                 END LOOP;
-
-				--Do we have all informations needed ?
+                servicesrow := NULL;
+                --Do we have all informations needed ?
                 IF ( ((cur->'hostname') IS NOT NULL) AND ((cur->'servicedesc') IS NOT NULL) AND ((cur->'label') IS NOT NULL) AND ((cur->'timet') IS NOT NULL) AND ((cur->'value') IS NOT NULL) ) THEN
                     --Does the service exists ?
-    				SELECT id INTO service_id
-    				FROM wh_nagios.services
-    				WHERE hostname = (cur->'hostname')
-    					AND service = (cur->'servicedesc')
-    					AND label = (cur->'label');
-				
-    				IF NOT FOUND THEN
-    					-- The trigger on wh_nagios.services will create the partition counters_detail_$service_id automatically
-    					INSERT INTO wh_nagios.services (id,hostname,warehouse,service,label,seracl,unit,state,min,max,critical,warning)
-    					VALUES (default,cur->'hostname','wh_nagios',cur->'servicedesc',cur->'label','{}'::aclitem[],cur->'uom',cur->'servicestate',(cur->'min')::numeric,(cur->'max')::numeric,(cur->'critical')::numeric,(cur->'warning')::numeric)
-    					RETURNING id INTO STRICT service_id;
-    				END IF;
-    				BEGIN
-    					EXECUTE format('INSERT INTO wh_nagios.counters_detail_%s (timet,value) VALUES (TIMESTAMP WITH TIME ZONE ''epoch''+%L * INTERVAL ''1 second'', %L );',service_id,cur->'timet',cur->'value');
-    				EXCEPTION
-    					WHEN OTHERS THEN
-    				END;
+                    SELECT * INTO servicesrow
+                    FROM wh_nagios.services
+                    WHERE hostname = (cur->'hostname')
+                        AND service = (cur->'servicedesc')
+                        AND label = (cur->'label');
+
+                    IF NOT FOUND THEN
+                        -- The trigger on wh_nagios.services will create the partition counters_detail_$service_id automatically
+                        INSERT INTO wh_nagios.services (id,hostname,warehouse,service,label,seracl,unit,state,min,max,critical,warning)
+                        VALUES (default,cur->'hostname','wh_nagios',cur->'servicedesc',cur->'label','{}'::aclitem[],cur->'uom',cur->'servicestate',(cur->'min')::numeric,(cur->'max')::numeric,(cur->'critical')::numeric,(cur->'warning')::numeric)
+                        RETURNING id INTO STRICT service_id;
+                        EXECUTE format('UPDATE wh_nagios.services SET oldest_record = timestamp with time zone ''epoch''+%L * INTERVAL ''1 second''',(cur->'timet'));
+                    ELSE
+                        --Do we need to update the service ?
+                        IF ( (servicesrow.last_modified + '1 day'::interval < CURRENT_DATE)
+                            OR ( (cur->'servicestate') IS NOT NULL AND (servicesrow.state <> (cur->'servicestate') OR servicesrow.state IS NULL) )
+                            OR ( (cur->'min') IS NOT NULL AND (servicesrow.min <> (cur->'min')::numeric OR (servicesrow.min IS NULL)) ) 
+                            OR ( (cur->'max') IS NOT NULL AND (servicesrow.max <> (cur->'max')::numeric OR (servicesrow.max IS NULL)) )
+                            OR ( (cur->'warning') IS NOT NULL AND (servicesrow.warning <> (cur->'warning')::numeric OR (servicesrow.warning IS NULL)) )
+                            OR ( (cur->'critical') IS NOT NULL AND (servicesrow.critical <> (cur->'critical')::numeric OR (servicesrow.critical IS NULL)) )
+                            OR ( (cur->'unit') IS NOT NULL AND (servicesrow.unit <> (cur->'unit') OR (servicesrow.unit IS NULL)) )
+                            OR (servicesrow.newest_record +'5 minutes'::interval < now() )
+                        ) THEN
+                            EXECUTE format('UPDATE wh_nagios.services SET last_modified = CURRENT_DATE,
+                                state = %s,
+                                min = %L,
+                                max = %L,
+                                warning = %L,
+                                critical = %L,
+                                unit = %s,
+                                newest_record= timestamp with time zone+%L * INTERVAL ''1 second''
+                            WHERE id = servicesrow.id',(cur->'servicestate'),(cur->'min'),(cur->'max'),(cur->'warning'),(cur->'critical'),(cur->'unit'),(cur->'timet'));
+                        END IF;
+                        service_id := servicesrow.id;
+                    END IF;
+
+                    IF (servicesrow IS NOT NULL AND servicesrow.last_cleanup < now() - '10 days'::interval) THEN
+                        PERFORM wh_naios.cleanup_partition(servicesrow.id,now()- '7 days'::interval);
+                    END IF;
+
+                    BEGIN
+                        EXECUTE format('INSERT INTO wh_nagios.counters_detail_%s (date_records,records) VALUES (date_trunc(''day'',timestamp with time zone ''epoch''+%L * INTERVAL ''1 second''),array[row(timestamp with time zone ''epoch''+%L * INTERVAL ''1 second'',%L )]::wh_nagios.counters_detail[]);',service_id,cur->'timet',cur->'timet',cur->'value');
+                    EXCEPTION
+                        WHEN OTHERS THEN
+                            IF (log_error = TRUE) THEN
+                                INSERT INTO wh_nagios.hub_reject (id, data,msg) VALUES (r_hub.id, r_hub.data, 'Error during INSERT: ' || SQLSTATE || '-' || SQLERRM);
+                            END IF;
+                    END;
+                ELSE
+                    IF (log_error = TRUE) THEN
+                        msg_err := NULL;
+                        IF ((cur->'hostname') IS NULL) THEN
+                            msg_err := COALESCE(msg_err,'') || 'hostname required';
+                        END IF;
+                        IF ((cur->'servicedesc') IS NULL) THEN
+                            msg_err := COALESCE(msg_err || ',','') || 'servicedesc required';
+                        END IF;
+                        IF ((cur->'label') IS NULL) THEN
+                            msg_err := COALESCE(msg_err || ',','') || 'label required';
+                        END IF;
+                        IF ((cur->'timet') IS NULL) THEN
+                            msg_err := COALESCE(msg_err || ',','') || 'timet required';
+                        END IF;
+                        IF ((cur->'value') IS NULL) THEN
+                            msg_err := COALESCE(msg_err || ',','') || 'value required';
+                        END IF;
+
+                        INSERT INTO wh_nagios.hub_reject (id, data,msg) VALUES (r_hub.id, r_hub.data, msg_err);
+                    END IF;
                 END IF;
-    		END IF;
-			--Delete current line
-			DELETE FROM wh_nagios.hub WHERE CURRENT OF c_hub;
-		END LOOP;
-	EXCEPTION
-		WHEN lock_not_available THEN
-			--Have frendlier exception if concurrent function already running
-			RAISE EXCEPTION 'Concurrent function already running.';
-	END;
-	RETURN true;
+            ELSE
+                IF (log_error = TRUE) THEN
+                    msg_err := NULL;
+                    IF (array_upper(r_hub.data,2) IS NOT NULL) THEN
+                        msg_err := COALESCE(msg_err,'') || 'tab more than 1 dimension';
+                    END IF;
+                    IF (array_upper(r_hub.data,1) <= 9) THEN
+                        msg_err := COALESCE(msg_err || ',','') || 'less than 10 values';
+                    END IF;
+                    IF ((array_upper(r_hub.data,1) % 2) != 0) THEN
+                        msg_err := COALESCE(msg_err || ',','') || 'number of parameter not even';
+                    END IF;
+
+                    INSERT INTO wh_nagios.hub_reject (id, data,msg) VALUES (r_hub.id, r_hub.data, msg_err);
+                END IF;
+            END IF;
+            --Delete current line
+            DELETE FROM wh_nagios.hub WHERE CURRENT OF c_hub;
+        END LOOP;
+    EXCEPTION
+        WHEN lock_not_available THEN
+            --Have frendlier exception if concurrent function already running
+            RAISE EXCEPTION 'Concurrent function already running.';
+    END;
+    RETURN true;
 END;
 $$;
 
-ALTER FUNCTION wh_nagios.dispatch_record() OWNER TO pgfactory;
+ALTER FUNCTION wh_nagios.dispatch_record(boolean) OWNER TO pgfactory;
+
+/* wh_nagios.cleanup_partition(bigint,timestamptz)
+Aggregate all data by day in an array, to avoid space overhead. It also delete consecutive rows with same value between the two bounds.
+
+@p_partid: ID of partition to cleanup.
+@p_max_timestamp: Use to specify which rows to analyze for deleting. Will happen between last_cleanup and this parameter.
+@return : true if everything went well.
+*/
+CREATE OR REPLACE FUNCTION cleanup_partition(p_partid bigint, p_max_timestamp timestamp with time zone) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+  c_tmp refcursor;
+  r_tmp record;
+  v_current_value numeric;
+  v_start_range timestamptz;
+  v_partname text;
+  v_previous_timet timestamptz;
+  v_counter integer;
+  v_previous_cleanup timestamptz;
+  v_cursor_found boolean;
+  v_oldest timestamptz;
+  v_newest timestamptz;
+BEGIN
+    SELECT last_cleanup INTO v_previous_cleanup FROM wh_nagios.services WHERE id = p_partid;
+
+    IF NOT FOUND THEN
+        RETURN false;
+    END IF;
+    v_partname := 'counters_detail_' || p_partid;
+
+    EXECUTE 'LOCK TABLE wh_nagios.' || v_partname;
+    EXECUTE 'CREATE TEMP TABLE tmp AS SELECT (unnest(records)).* FROM wh_nagios.'|| v_partname;
+
+    SELECT min(timet),max(timet) INTO v_oldest,v_newest FROM tmp;
+
+    OPEN c_tmp FOR EXECUTE 'SELECT timet,value FROM tmp WHERE timet >= ' || quote_literal(v_previous_cleanup) || ' AND timet <= ' || quote_literal(p_max_timestamp) || ' ORDER BY timet';
+    LOOP
+        FETCH c_tmp INTO r_tmp;
+        v_cursor_found := FOUND;
+        v_counter := v_counter+1;
+        IF (v_cursor_found AND v_current_value IS NULL) THEN
+            v_current_value := r_tmp.value;
+            v_start_range := r_tmp.timet;
+            v_counter := 1;
+        ELSIF (NOT v_cursor_found OR v_current_value <> r_tmp.value) THEN
+            IF (v_counter>= 4) THEN
+                RAISE DEBUG 'DELETE BETWEEN % and % on partition %, counter=%',v_start_range,v_previous_timet,p_partid,v_counter;
+                EXECUTE 'DELETE FROM tmp WHERE timet > $1 AND timet < $2' USING v_start_range,v_previous_timet;
+            END IF;
+            EXIT WHEN NOT v_cursor_found;
+
+            v_start_range := r_tmp.timet;
+            v_current_value := r_tmp.value;
+            v_counter := 1;
+        END IF;
+        v_previous_timet := r_tmp.timet;
+    END LOOP;
+    CLOSE c_tmp;
+
+    RAISE DEBUG 'truncate wh_nagios.%',v_partname;
+    EXECUTE 'TRUNCATE wh_nagios.' || v_partname;
+    EXECUTE 'INSERT INTO wh_nagios.' || v_partname || ' SELECT date_trunc(''day'',timet),array_agg(row(timet,value)::wh_nagios.counters_detail) FROM tmp GROUP BY date_trunc(''day'',timet)';
+    EXECUTE 'DROP TABLE tmp';
+
+    UPDATE wh_nagios.services SET last_cleanup = p_max_timestamp, oldest_record = v_oldest, newest_record = v_newest
+        WHERE id = p_partid;
+    RETURN true;
+END;
+$$;
+
+ALTER FUNCTION wh_nagios.cleanup_partition(bigint, timestamp with time zone) OWNER TO pgfactory;
 
 --Automatically create a new partition when a service is added.
 CREATE OR REPLACE FUNCTION wh_nagios.create_partition_on_insert_service() RETURNS trigger
-	LANGUAGE plpgsql
-	AS $$
+    LANGUAGE plpgsql
+    AS $$
 BEGIN
-	EXECUTE 'CREATE TABLE wh_nagios.counters_detail_' || NEW.id || ' (timet timestamptz primary key, value numeric)';
-	RETURN NEW;
+    EXECUTE 'CREATE TABLE wh_nagios.counters_detail_' || NEW.id || ' (date_records date, records wh_nagios.counters_detail[])';
+    RETURN NEW;
 EXCEPTION
-	WHEN duplicate_table THEN
-		EXECUTE 'TRUNCATE TABLE wh_nagios.counters_detail_' || NEW.id;
-		RETURN NEW;
+    WHEN duplicate_table THEN
+        EXECUTE 'TRUNCATE TABLE wh_nagios.counters_detail_' || NEW.id;
+        RETURN NEW;
 END;
 $$;
 
@@ -112,14 +279,14 @@ ALTER FUNCTION wh_nagios.create_partition_on_insert_service() OWNER TO pgfactory
 
 --Automatically delete a partition when a service is removed.
 CREATE OR REPLACE FUNCTION wh_nagios.drop_partition_on_delete_service() RETURNS trigger
-	LANGUAGE plpgsql
-	AS $$
+    LANGUAGE plpgsql
+    AS $$
 BEGIN
-	EXECUTE 'DROP TABLE wh_nagios.counters_detail_' || OLD.id;
-	RETURN NULL;
+    EXECUTE 'DROP TABLE wh_nagios.counters_detail_' || OLD.id;
+    RETURN NULL;
 EXCEPTION
-	WHEN undefined_table THEN
-		RETURN NULL;
+    WHEN undefined_table THEN
+        RETURN NULL;
 END;
 $$;
 
